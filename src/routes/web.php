671a1f7ae3dfa6f07af86e52c27a785eea084f97
gurinder-batth <gurinderbatth08@gmail.com>
2025-06-21 17:36:36 +0530

    add a horizon support

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Jobs\TestHorizonJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-job', function () {
+    TestHorizonJob::dispatch();
+    return 'Job dispatched!';
 });
